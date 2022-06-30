10aff3aa1751c0a86c6f0fb0e9755cd290a2e25c
your name <you@example.com>
2022-06-30 14:05:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 
 #include <stdio.h>
 /**
- *  * main - Prints string
- *   * Description: Prints "\"Programming is like building a multilingual puzzle"
- *    * Return: 0
- *     */
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
