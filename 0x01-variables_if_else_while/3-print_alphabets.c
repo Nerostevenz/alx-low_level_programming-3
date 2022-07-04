dff6c38735ed15dbbc8adddd97ac849c407ff174
your name <you@example.com>
2022-07-04 12:03:21 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,18 @@
  * main - prints the alphabet in lowercase, and then in uppercase
  * return: 0
  */
-int main() { char a = 'a';
-while (a <= 'z')
-{putchar(a);
-a++;
-}
-a = 'A';
-while a <= 'Z'
-{putchar(a);
-a++;
-}
-putchar('\n');
-return(0);
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{putchar(a);
+		a++;
+	}
+	a = 'A';
+	while a <= 'Z'
+	{putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return(0);
 }
