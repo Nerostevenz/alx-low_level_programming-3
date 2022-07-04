f0b0f5dbe6c53a7b5d09b6f36311812547a2b91c
your name <you@example.com>
2022-07-04 11:08:37 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,13 @@
  * main - prints out the alphabet in lowercase
  * Return: 0
  */
-int main(void){
+int main(void) {
 	char a = 'a';
 
         while (a < 'z');
  
 	putchar(a);
-	a++;	
-
-   putchar('\n');
+	a++; {
+		putchar('\n');}
 	return (0);
 }
