f26e42c058e952c78229474855a9733a10cceb59
your name <you@example.com>
2022-07-04 14:57:12 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success/completed)
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
+ * Return: 0
  */
-
 int main(void)
 {
-	int number_left;
-	int number_right;
-	for (number_left = 48; number_right <= 78; number_left++)
+	int i, j;
+
+	for (i = 48; i < 58; i++)
 	{
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-		{
-			putchar(number_left);
-			putchar (number_right);
-			if ((number_left == 56) && (number_right == 78))
-			{
-				break;
-			}
-			putchar(',');
-			putchar (' ');
-		}
+	for (j = i; j < 58; j++)
+	{if (i == j)
+	{continue;
+	putchar(i);
+	putchar(j);
+	if (i == 56 && j == 57)
+	{break;
+	}
+		else
+	{putchar(',');
+		putchar(' ')
+	}
 	}
 	putchar('\n');
 	return (0);
