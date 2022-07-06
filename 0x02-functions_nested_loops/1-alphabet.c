95a1427a5d80a56a914a8d356b61a477feab18c7
your name <you@example.com>
2022-07-06 03:33:01 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print -alphabet in lowercase
+ * print_alphabet - print alphabet in lowercase
  *
- * Return: 0 
+ * Return: Always 0 
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i = 97; i <= 122; i++);
 	{
 		_putchar(i);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,4 +2,7 @@
 
 ## 0. _putchar
 Program that prints "_putchar", followed by a new line.
+prints the alphabet, in lowercase, followed by a new line.
+
+
 
