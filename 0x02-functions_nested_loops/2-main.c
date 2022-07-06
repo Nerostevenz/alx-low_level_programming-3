fed3a171b11862b330f6bd6cb7160e0930e53034
your name <you@example.com>
2022-07-06 06:13:24 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"                                                                                                     
+/**
+ * main - calls other function
+ * 
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	_putchar();
+	
+	return (0);
+}
