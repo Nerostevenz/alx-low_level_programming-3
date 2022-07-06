aef858064f6f41a32d6752c7ff54b9ba67bb146b
your name <you@example.com>
2022-07-06 10:15:56 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,15 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char c;
-	 while (i < 10)
- {
-	 c = 'a';
-	 while (c <= 'z')
- {
-	 _putchar(c);
-	 c++;
- }
-	 _putchar('\n');
-	 i++;
- }
+       	while (i < 10)
+	{
+	       	c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
