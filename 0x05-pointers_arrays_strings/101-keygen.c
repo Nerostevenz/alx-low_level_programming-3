8910ec09bad63d3d3b6cacb28755bd48635b3508
your name <you@example.com>
2022-07-12 03:21:35 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,18 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
+#define MAX 125
+#define MIN 1
 /**
- *
- *
+ * main - main
+ * Return: 0
  */
 
 int main(void)
 {
-	int num;
-
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
+	int rnd;
+	int count;
+	int total;
+	srand(time(NULL));
+	
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		rnd = (rand() % (MAX + 1 - MIN)) + MIN;
+		rnd = (rand() % 125) + 1;
+		printf("%c", rnd);
+		total -= rnd;
+	}
+	printf("%c", total);
 	return (0);
 }
