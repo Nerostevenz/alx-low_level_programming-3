9de337d1b656d23114706e24ca7bedcd047cf9d7
your name <you@example.com>
2022-07-12 02:41:14 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,10 @@
-
 #include "main.h"
 /**
- *  * rev_string - prints a reverse string
- *   *@s: A pointer to an int that will be changed
- *    *
- *     *Return: void
- *      */
+ * rev_string - prints a reverse string
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void
+ */
 
 void rev_string(char *s)
 {
@@ -23,17 +22,13 @@ void rev_string(char *s)
 	start_c = s;
 	end_c = s;
 
-
 	for (i = 0; i < count - 1; i++)
 	{
 		end_c++;
 	}
 
-
 	for (i = 0; i < count / 2; i++)
 	{
-
-
 		c = *end_c;
 		*end_c = *start_c;
 		*start_c = c;
