0914ae5f368e911f6ca72b22f0e4efcc1b343143
your name <you@example.com>
2022-07-18 14:55:06 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,29 +1,29 @@
 #include "main.h"
-/*
+
+/**
  * _strpbrk - matches any character specified
- * @s: This is the C string to be scanned
+ * @s: This is the C string to be scanned.
  * @accept:character in str1 that matches one of the characters in str2
- * Return: string s that matches any character specified in accept
+ *Return: string s that matches any character specified in accept
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
 
-	while (*s != '\0')
+	while (*s != '\0') /*Declaring WHILE*/
 	{
 		j = 0;
-		while (accept[j] != '\0')
+		while (accept[j] != '\0')  /*Evaluating *accept*/
 		{
 			if (*s == accept[j])
 			{
 				return (s);
 			}
 
-			j++;
+			j++; /*add j+1*/
 		}
-		s++;
-
+		s++; /*add s+1*/
 	}
 	return (0);
 }
