94df6f0cd0c2a3d839f1a79c6c22a31f1c149a44
your name <you@example.com>
2022-07-20 12:05:55 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  *_strlen_recursion - length of a string
- *   *@s: pointer block of memory to fill
- *    *Return: strlen_recursion
- *     */
+ *_strlen_recursion - length of a string
+ *@s: pointer block of memory to fill
+ *Return: strlen_recursion
+ */
 
 int _strlen_recursion(char *s)
 {
