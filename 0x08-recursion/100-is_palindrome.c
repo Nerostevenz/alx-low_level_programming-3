43e6cac127e69e7f38ea93cdefd2162fc214178b
your name <you@example.com>
2022-07-20 12:40:12 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 	{
 		return (0);
 	}
-	
+
 	return (1 + _strlen_recursion(++s));
 }
 
@@ -29,12 +29,12 @@ int p1(char *s, int l)
 	{
 		return (1);
 	}
-	
+
 	if (*s == *(s + l))
 	{
 		return (p1(s + 1, l - 2));
 	}
-	
+
 	return (0);
 }
 
@@ -47,6 +47,6 @@ int p1(char *s, int l)
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
-	
+
 	return (p1(s, len - 1));
 }
